d44d2be07140d3566af29eb7dc4e081d0072af64
EC2 Default User <ec2-user@ip-172-31-62-105.ec2.internal>
2021-04-05 10:19:03 +0000

    new book

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,21 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books.all
+    @book　= Book.new
+  end
+
+  def create
+    @book = Book.new
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
