ddf4f6d4d2a5cf3b6b10fc068f26f9b4d85571ea
EC2 Default User <ec2-user@ip-172-31-62-105.ec2.internal>
2021-04-05 09:32:49 +0000

    top,aboutページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'books/index'
-  get 'books/show'
-  get 'users/index'
-  get 'users/show'
   devise_for :users
 
   root to: 'homes#top'
+  
+  get'about'=> 'homes#about'
+
+  resources :books
+
+  resources :users
 
 end
 
